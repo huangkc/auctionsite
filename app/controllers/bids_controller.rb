8f749d31709d68ef81ba1664ef2f0671edb1710c
Kai Huang <kaihuang@kais-air.attlocal.net>
2016-05-13 11:51:17 -0700

    modify stylesheets

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,3 +1,11 @@
+# index
+# get '/bids' do 
+# end
+
+# new
+# get '/bids/new' do
+# end
+
 post '/bids' do
   @item_id = params[:id]
   @bid = Bid.new(bid_price: params[:bid_price], item_id: params[:id], user_id: current_user.id)
@@ -8,3 +16,26 @@ post '/bids' do
 	 @errors = @bid.errors.full_messages
    end
 end
+
+# show
+# get '/bids/:id' do 
+# end
+
+# edit
+# get '/bids/:id/edit/' do 
+# end
+
+# update
+# put 'bids/:id' do 
+		# @bid = Bid.find(params[:id])
+#   @bid.update(params[:bid])
+#   redirect "/users/#{@user.id}"
+# end
+
+# delete
+# delete '/bids/:id' do
+	# bids = Bid.find(params[:id])
+	# bids.destroy
+	# redirect '/'
+# end
+
